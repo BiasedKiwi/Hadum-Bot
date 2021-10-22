a2899e149755b657399d375230eb4eb9ecbae134
Hentyhentys <alexandre.turellier@icloud.com>
2021-10-22 10:32:46 +0300

    Added test for get_token() function

--- src/tests/test_startup.py
+++ src/tests/test_startup.py
@@ -1,13 +1,14 @@
-from utils import startup
+import os
 import unittest
-from discord.ext import commands
 
+from discord.ext import commands
+from utils import startup
 
 client = commands.Bot(command_prefix="", help_command=None)
 
 
 class TestStartup(unittest.TestCase): 
-    def test_load_cogs(self):
+    def test_load_cogs_recursively(self):
         self.assertEqual(startup.load_cogs(client, include_folders=True), 0)
         
     def test_initial_load_with_debug_off(self):
@@ -15,6 +16,18 @@ class TestStartup(unittest.TestCase):
         
     def test_initial_load_with_debug_on(self):
         self.assertEqual(startup.initial_load(debug=True), 0)
+        
+    def test_get_token(self):
+        try:
+            with open("./.env", "x") as outfile:
+                print("sample .env file created")
+                outfile.seek(0)
+                outfile.write("TOKEN=12345")
+        except FileExistsError:
+            pass
+        self.assertNotEqual(startup.get_token(file="./.env",ask_for_token=False), -1)
+        if os.path.exists("./.env"):
+            os.remove("./.env")        
         
         
 if __name__ == "__main__":
