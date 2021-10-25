d7e1797eebdaac0ecff6a23209a9494e11d29f43
shadawcraw <alexandre1920911@gmail.com>
2021-10-25 11:10:26 +0300

    deleted install.sh

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [[ "$OSTYPE" == "win32"* ]]; then
-        python -m pip install /r requirements.txt
-else
-        python3 -m pip3 install -r requirements.txt
-fi
-
-touch ../.env
