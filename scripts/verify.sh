78b06e1ca20d5e74a06cbfa3ecc0f2985045ea70
shadawcraw <alexandre1920911@gmail.com>
2021-12-03 16:00:31 +0300

    updated checksums

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # This Script verifies the integrity of files using SHA256 checksums: this means that if you modified
 # the files, this test will fail since the checksums have been modified. This is normal.
-# Checksums generated on 21/10/21
+# Checksums generated on 03/12/21
 echo Verifying integrity of files...
 cd ../
 sha256sum -c scripts/checksums.txt || echo "Test failed! This may happen if you modified the files."
