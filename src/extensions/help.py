7f4120e92c576f59d90c2031b7003a070ce5b734
shadawcraw <alexandre1920911@gmail.com>
2021-12-04 13:42:34 +0200

    "Renovated" the help command

--- src/extensions/help.py
+++ src/extensions/help.py
@@ -11,34 +11,43 @@ class Help(commands.Cog):
         self.bot = bot
         console.log(__name__.strip("extensions.") + " Cog Online")
         
-        
     @commands.group(name="help", aliases=["cmds", "commands"])
     async def help_(self, ctx: commands.Context):
+        def gen_field(name, value): return embed.add_field(name=name, value=value, inline=True)
+        
         if ctx.invoked_subcommand is None:
             embed = nextcord.Embed(title="Hadum Commands")
-            embed.add_field(name="Moderation", value="Run `h.help moderation` to see commands.", inline=True)
-            embed.add_field(name="Miscellaneous", value="Run `h.help misc` to see commands.", inline=True)
+            gen_field("Moderation", "Run `h.help moderation` to see commands.")
+            gen_field("Miscellaneous", "Run `h.help misc` to see commands.")
+            gen_field("Reddit", "Run `h.help reddit` to see commands.")
             await ctx.channel.send(embed=embed)
             
             return 0
         
-    
     @help_.command(name="moderation")
     async def moderation(self, ctx: commands.Context):
         embed = nextcord.Embed(title="Moderation Commands")
-        embed.add_field(name="ban", value="Permanently Ban a user. Usage: `h.ban [USER] [REASON]`", inline=False)
-        embed.add_field(name="kick", value="Kick a user. Usage: `h.kick [USER] [REASON]`", inline=False)
-        embed.add_field(name="masskick", value="Kick multiple users simultaneously. Usage: `h.masskick [USERS] [REASON]`", inline=False)
+        embed.add_field(name="ban", value="Permanently Ban a user.\n Usage: `h.ban [USER] [REASON]`", inline=False)
+        embed.add_field(name="kick", value="Kick a user.\n Usage: `h.kick [USER] [REASON]`", inline=False)
+        embed.add_field(name="masskick", value="Kick multiple users simultaneously.\n Usage: `h.masskick [USERS] [REASON]`", inline=False)
         await ctx.channel.send(embed=embed)
         
         return 0
     
-    
     @help_.command(name="misc")
     async def misc(self, ctx: commands.Context):
         embed = nextcord.Embed(title="Miscellaneous Commands")
-        embed.add_field(name="ping", value="Get the latency between the bot and Discord Servers. Usage: `h.ping`")
-        embed.add_field(name="coinflip", value="Flip a coin! Usage: `h.coinflip`")
+        embed.add_field(name="ping", value="Get the latency between the bot and Discord Servers.\n Usage: `h.ping`", inline=False)
+        embed.add_field(name="coinflip", value="Flip a coin!\n Usage: `h.coinflip`", inline=False)
+        await ctx.channel.send(embed=embed)
+        
+        return 0
+    
+    @help_.command(name="reddit")
+    async def reddit_(self, ctx: commands.Context):
+        embed = nextcord.Embed(title="Miscellaneous Commands")
+        embed.add_field(name="meme", value="Get a random meme!\n Usage: `h.meme`", inline=False)
+        embed.add_field(name="reddit", value="Get a random post from the subreddit entered!\n Usage: `h.reddit [SUBREDDIT]`", inline=False)
         await ctx.channel.send(embed=embed)
         
         return 0
