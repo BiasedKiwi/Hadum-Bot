f39ebed7c505cc6fae6bde96e3109f31462686d6
shadawcraw <61089773+shadawcraw@users.noreply.github.com>
2022-01-27 09:26:13 +0200

    Update script to install dependencies
    And added comments

--- install.sh
+++ install.sh
@@ -2,18 +2,20 @@
 
 echo "Starting Hadum install script..."
 
-sudo bash scripts/verify.sh
+bash scripts/verify.sh # Run integrity check script
+echo "Installing dependencies"
+pipenv install || pip3 install -r requirements.txt. # Install dependencies with pipenv, if it fails, use pip3 instead.
 
 echo "Find your Discord token here: https://discord.com/developers/applications/"
 echo "Find your Reddit info here: https://www.reddit.com/prefs/apps"
-echo "Please input your Discord token below"
+echo "Please input your Discord token below". # Get config info
 read DSC_TOKEN
 echo "Please input your Reddit Client secret below"
 read REDDIT_SECRET
 echo "Please input your Reddit Client ID below"
 read REDDIT_ID
 
-echo "Configuring environment variables..."
+echo "Configuring environment variables..." # Create environment variables
 echo "TOKEN="$DSC_TOKEN > .env
 echo "REDDIT_CLIENT_SECRET="$REDDIT_SECRET > .env
 echo "REDDIT_CLIENT_ID="$REDDIT_SECRET > .env
